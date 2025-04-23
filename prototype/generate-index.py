75b48e1236f23d1f42f90519c4204379c139126f
Arghya Dutta <argphy@gmail.com>
2025-04-23 11:11:38 +0530

    Last updated on 2025-04-23 11:11:38

--- prototype/generate-index.py
+++ prototype/generate-index.py
@@ -15,6 +15,7 @@ html = """<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>E3 Ligome</title>
     <link rel="stylesheet" href="assets/style.css">
     <script src="search.js" defer></script>
